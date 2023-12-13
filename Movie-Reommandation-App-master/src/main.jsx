a20dd7637ea9720b6b66a36016662d1e4b21411e
MitankshGosalia <140804855+MitankshGosalia@users.noreply.github.com>
2023-12-13 21:09:52 +0530

    Update main.jsx

--- Movie-Reommandation-App-master/src/main.jsx
+++ Movie-Reommandation-App-master/src/main.jsx
@@ -7,9 +7,10 @@ import store from "./app/store.js";
 import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // Once store is created, make it available to components by adding it in provider around app and passing as a prop
   <Provider store={store}>
-    {/* <Navbar /> */}
+    {
+      
+    }
     <App />
   </Provider>,
 );
